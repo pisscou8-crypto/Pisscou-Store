4251049a5116cf38bd02499561347ec44ba8640a
pisscou8-crypto <pisscou8@gmail.com>
2026-09-25 11:50:10 +0100

    Add initial main.cpp for Pisscou Store display

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Pisscou Store\n");
+    printf("\n");
+    printf("1. FPKGi\n");
+    printf("2. Apollo Save Tool\n");
+    printf("3. More Homebrew Apps\n");
+    printf("\n");
+    printf("Pisscou Store - PS4 Homebrew Library\n");
+
+    return 0;
